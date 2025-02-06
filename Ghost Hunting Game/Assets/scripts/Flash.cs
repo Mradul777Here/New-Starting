01e790b3f1eb2dc7ace8c6b7842152e60ce8d2b8
elifts <coolstuffvideo42@gmail.com>
2025-02-06 19:14:52 +1100

    a
    a

---
+++ Ghost Hunting Game/Assets/scripts/Flash.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Flash : MonoBehaviour
+{
+    public Light lightSource;
+    public float fadeDuration;
+    public float maxIntensity;
+    public bool LightFlashing;
+    public bool flashbang;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (!LightFlashing)
+            {
+
+                StartCoroutine(FadeOutLight());
+                LightFlashing = true;
+            }
+
+        }
+    }
+
+    IEnumerator FadeOutLight()
+    {
+        lightSource.intensity = maxIntensity;
+        float elapsedTime = 0f;
+        if (Random.Range(0, 10) == 0 && !flashbang)
+        {
+            lightSource.color = Color.red;
+        }
+        if (flashbang)
+        {
+            maxIntensity = 1000;
+
+        }
+
+        while (elapsedTime < fadeDuration)
+        {
+            lightSource.intensity = Mathf.Lerp(maxIntensity, 0, elapsedTime / fadeDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        lightSource.intensity = 0;
+        flashbang = false;
+        lightSource.color = Color.white;
+        yield return new WaitForSeconds(1);
+        LightFlashing = false;
+        maxIntensity = 500;
+    }
+}
---
+++ Ghost Hunting Game/Assets/scripts/Items.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEditor.ProBuilder;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Items : MonoBehaviour
+{
+    public GameObject item1;
+    public GameObject item2;
+    public bool gotItem1;
+    public LayerMask layerMask;
+    public float distance;
+    public GameObject posts;
+    public Slider a;
+    // See Order of Execution for Event Functions for information on FixedUpdate() and Update() related to physics queries
+    void Update()
+    {
+
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance, layerMask))
+
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("Did Hit");
+                if (hit.collider.gameObject == item1.gameObject)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    gotItem1 = true;
+                }
+                else if (hit.collider.gameObject == item2.gameObject && gotItem1)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+                Debug.Log("Did not Hit");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+    public void post()
+    {
+        if (a.value >= 0.5)
+        {
+            posts.SetActive(false);
+        }
+        else
+        {
+            posts.SetActive(true);
+        }
+    }
+}
